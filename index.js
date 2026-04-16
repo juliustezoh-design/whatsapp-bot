b2e0afdf15edd7ec4698fa37525adc0f82a7bc57
Calvin <juliustezoh@gmail.com>
2026-04-16 01:42:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
+
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState("auth");
+
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: true
+    });
+
+    sock.ev.on("creds.update", saveCreds);
+
+    sock.ev.on("messages.upsert", async (m) => {
+        const msg = m.messages[0];
+        if (!msg.message) return;
+
+        const text =
+            msg.message.conversation ||
+            msg.message.extendedTextMessage?.text;
+
+        console.log("Message:", text);
+
+        if (text && text.toLowerCase() === "hi") {
+            await sock.sendMessage(msg.key.remoteJid, {
+                text: "Hello 👋 I am your Railway bot"
+            });
+        }
+    });
+}
+
+startBot();
